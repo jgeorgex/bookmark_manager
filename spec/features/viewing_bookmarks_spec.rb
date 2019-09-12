0afb0a970d04b49313cde03c09dfb66b98da75e5
Julian George <jules104@gmail.com>
2019-09-12 14:42:36 +0100

    All tests now working

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -9,11 +9,11 @@ end
 
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    connection = PG.connect(dbname: 'bm_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(7, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(8, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(9, 'http://www.google.com');")
 
     visit('/bookmarks')
 
